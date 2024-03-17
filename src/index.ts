4bb71d531bb8b48e210fa4fe6e835332f26ee27b
Ramesh Thiruchelvam <ramesh@limarktech.com>
2024-03-17 20:02:43 +0530

    feat: get surface area

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,18 @@
-console.log("Hello, world!")
+function getSurfaceAreaOfSquare(side: number) {
+  return side * side;
+}
+
+function getSurfaceAreaOfRectangle(length: number, width: number) {
+  return length * width;
+}
+
+function getSurfaceAreaOfTriangle(base: number, height: number) {
+  return (base * height) / 2;
+}
+
+function getSurfaceAreaOfCircle(radius: number) {
+  return Math.PI * radius * radius;
+}
+
+console.log(getSurfaceAreaOfSquare(5));
+console.log(getSurfaceAreaOfCircle(5));
