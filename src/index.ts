f69457c69117ed5492ab6dd9e0aa56d854c1f9b6
Ramesh Thiruchelvam <ramesh@limarktech.com>
2024-03-17 19:56:32 +0530

    feat: greetings

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,27 @@
-console.log("Hello, world!")
+function sayHello(name: string) {
+  console.log(`Hello ${name}!`)
+}
+
+function greet(name: string) {
+  const time = new Date().getHours();
+
+  if (time < 12) {
+    console.log(`Good Morning ${name}!`);
+  } else if (time < 18) {
+    console.log(`Good Afternoon ${name}!`);
+  } else {
+    console.log(`Good Evening ${name}!`);
+  }
+}
+
+function getAge(dateOfBirth: Date) {
+  const today = new Date();
+  const age = today.getFullYear() - dateOfBirth.getFullYear();
+  return age;
+}
+
+greet("Amirthan");
+sayHello("Akila");
+
+
+console.log(getAge(new Date(2011, 8, 10)));
